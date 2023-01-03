459049ba34cc1566b2711e7bcf97daf7c98cd122
Jakub Bąba <jakub.baba.stud@pw.edu.pl>
2023-01-03 19:26:27 +0100

    fixes for general board class

--- boards.py
+++ boards.py
@@ -9,16 +9,16 @@ class Board:
     def __init__(self, size=3):
         self._active = True
         self._size = size
-        self._fields = []
+        self.fields = []
 
         if size < 3 or size % 2 == 0:
             raise SizeError("Size has to be an odd number greater than one")
 
         # creating board fields
         for i in range(size):
-            self._fields.append([])
+            self.fields.append([])
             for _ in range(size):
-                self._fields[i].append(None)
+                self.fields[i].append(None)
 
     @property
     def active(self):
@@ -31,33 +31,42 @@ class Board:
             self._active = False
 
     def make_move(self, x, y, player):
-        if self._active and self._fields[x][y] is None:
-            self._fields[x][y] = player
+        if self._active and self.fields[x][y] is None:
+            self.fields[x][y] = player
+            self.check_winner()
 
     def check_winner(self):
         def winner_state(fields):
-            if set(fields) == 1 and None not in fields:
+            if len(set(fields)) == 1 and None not in fields:
                 self._active = False
                 return True
             return False
 
-        for row in range(self._size):
+        for row in self.fields:
             if winner_state(row):
                 return row[0]
 
         for i in range(self._size):
-            column = [self._fields[j][i] for j in range(self._size)]
+            column = [self.fields[j][i] for j in range(self._size)]
             if winner_state(column):
                 return column[0]
 
-        diagonal = [self._fields[i][j] for (i, j)
+        diagonal = [self.fields[i][j] for (i, j)
                     in zip(range(self._size), range(self._size))]
         if winner_state(diagonal):
             return diagonal[0]
-        diagonal = [self._fields[i][j] for (i, j)
+        diagonal = [self.fields[i][j] for (i, j)
                     in zip(range(self._size), range(self._size-1, -1, -1))]
         if winner_state(diagonal):
             return diagonal[0]
+
+        for row in self.fields:
+            for field in row:
+                if field is None:
+                    return None
+
+        self._active = False
+        return "tie"
 
 
 class BigBoard(Board):
